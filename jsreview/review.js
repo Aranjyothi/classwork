d6df487fa88986c7088b2ae08046ba04a0fb5ccd
jyothipropelo <98364003+jyothipropelo@users.noreply.github.com>
2022-08-18 15:19:11 -0400

    review

---
+++ jsreview/review.js
@@ -0,0 +1,24 @@
+//I. variables& Datatypes//
+//a1- The equal will assign a value to a variable. ex- (let B = 22)
+//a2- Will need to be resigned with a new value.
+//a3- We will combine each variable to new variable. 
+//a4- Declare , Assign , Define ex -( let X = 22) value
+//a5- pseudcoding -writing out the syntax or explaination of the problem. It helps you set up the syntax. 
+//a6- 70/30 percent should be spent.
+
+//b1-b2- const firstVariable = 'Hello World'
+//b3- let firstVariable = 22
+//b4- let firstVariable = secondVariable
+//b5- let secondVariable = 'hey'
+//b6- hey(let b = 'hi';
+        // let a = b;
+        //let c = a;
+        // console.log(a);)
+// let secondVariable = 'hey';
+// let firstVariable = secondVariable;
+// console.log(firstVariable)
+//b7- 
+// let yourName = 'Marleni'
+// console.log('Hello, my name is ' + yourName)
+
+
